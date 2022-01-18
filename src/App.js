3ff622f0d3242606d3d8431bcf946e7a9fc33944
ornelas91 <anderson_ornelas91@hotmail.com>
2022-01-17 21:26:20 -0300

    Atualizando

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import Item from './components/Item';
+
 const App = () => {
   return(
-    <h1>Hello World</h1>
+    <>
+      <h1>Aplicaçõa com React</h1>
+      <ul>
+        <Item>
+          Item 1
+        </Item>
+        <Item>
+          Item 2
+        </Item>
+        <Item>
+          Item 3
+        </Item>
+      </ul>
+    </>
   )
 }
 
