4351f7ecdc5334b26d5d70f4cefde35ad01f14ca
ornelas91 <anderson_ornelas91@hotmail.com>
2022-01-17 21:47:19 -0300

    Atualizando

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Item from './components/Item';
+import Card from './components/Card';
 
 const App = () => {
   return(
@@ -15,6 +16,7 @@ const App = () => {
           Item 3
         </Item>
       </ul>
+      <Card/>
     </>
   )
 }
